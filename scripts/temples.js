0e32b6666fe0a64f1a0050aa47eb96c754284879
Frankool <etimfrank@gmail.com>
2025-09-11 17:16:02 +0100

    temples.htlm1

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+// Footer year + last modified
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Hamburger menu
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+  navigation.style.display = navigation.style.display === "flex" ? "none" : "flex";
+  menuButton.textContent = menuButton.textContent === "✖" ? "☰" : "✖";
+});
