d4e6555171bae9e140231df8ba09f6b512e847c3
Frankool <etimfrank@gmail.com>
2025-09-12 23:55:26 +0100

    temples.html1c

--- scripts/temples.js
+++ scripts/temples.js
@@ -19,3 +19,16 @@ function closeNav() {
   document.getElementById("mySideNav").style.width = "0";
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
+
+
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  document.getElementById("nav-links").classList.toggle("show");
+});
