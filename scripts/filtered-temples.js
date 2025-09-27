1bc59b4cc38f4fbb95db2cd75ccca11e0cdd218a
Frankool <etimfrank@gmail.com>
2025-09-27 12:34:48 +0100

    filtered-temples.html1c

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -8,7 +8,7 @@ const navigation = document.querySelector("#nav-links");
 
 menuButton.addEventListener("click", () => {
   navigation.classList.toggle("show");
-  menuButton.textContent = menuButton.textContent === "✖" ? "☰" : "✖";
+  menuButton.textContent = menuButton.textContent("show") ? "☰" : "✖";
 });
 
 function openNav() {
